e2f419849face30e19ad3c81166ad029aea6e620
Gdawson771 <gdawson771@gmail.com>
2021-04-11 17:36:44 +0100

    Created flickering light

---
+++ Assets/lightFlicker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class lightFlicker : MonoBehaviour
+{
+    private Light2D light;
+    private CircleCollider2D collider;
+    private float flickerStartTime;
+
+    private float flickerDuration;
+    //Get the point light and store that locally here
+    //GET 2d box COLLIDER AND STORE THAT HERE
+    //set variable for how many flickers per second
+    //in update get random number, disable everything, enable after the flicker time 
+    void Start()
+    {
+        light = gameObject.GetComponent<Light2D>();
+        collider = gameObject.GetComponent<CircleCollider2D>();
+        flickerDuration = 3f;
+        flickerStartTime=Time.time;
+
+    }
+
+    //get a random number store this locallyoutside update
+    //time.deltatime figure out how to count time in update
+    //after time has passed renable
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time>(flickerStartTime+flickerDuration))
+        {
+            flickerDuration = Random.Range(0.2f, 2f);
+            flickerStartTime=Time.time;
+            light.enabled = !light.enabled;
+        }
+        Debug.Log(Time.time);
+    }
+}
